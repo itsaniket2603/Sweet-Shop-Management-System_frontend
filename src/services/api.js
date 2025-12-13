fc8bc886f04391915d4827fdf207103892982dc9
Aniket <its.aniket26@gmail.com>
2025-12-13 17:13:52 +0530

    Fix: remove express from frontend api

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://sweet-shop-management-system-backend-5.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
-  withCredentials: false, // 🔥 MUST be false
+  withCredentials: false,
 });
 
 api.interceptors.request.use((config) => {
